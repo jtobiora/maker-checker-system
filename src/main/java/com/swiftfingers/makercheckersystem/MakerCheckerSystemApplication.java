ee9ba7a7c6b2d5da73dd13cae9ba649a9e05dee3
Obiora <jt.banego@gmail.com>
2024-05-04 18:27:24 +0100

    Initial commit

---
+++ src/main/java/com/swiftfingers/makercheckersystem/MakerCheckerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.swiftfingers.makercheckersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakerCheckerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakerCheckerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swiftfingers/makercheckersystem/MakerCheckerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftfingers.makercheckersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakerCheckerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
